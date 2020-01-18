fe38e8d968f07d344e78aea0e13e29071c11749b
wifimug <44526624+wifimug@users.noreply.github.com>
2020-01-18 21:46:16 +0000

    please take over from this not very hot css mess

---
+++ js/center.js
@@ -0,0 +1,12 @@
+function centeredText() {
+    var centerTag = 
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    //===== Start up the background animation. =====
+    //Work out how many lines are needed.
+    backgroundAnimate(10, 30);
+
+    //Shows screen
+    showScreen(0);
+});
