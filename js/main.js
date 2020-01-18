71a3d0b119738300bebe77f006d65bc5ee3d8bda
rubyleehs <rubyleehs@live.com>
2020-01-18 14:21:54 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-const resourceType = {
-    BOT: 'bot',
-    SERVER: 'server',
-    MAINFRAME: 'mainframe',
-    CASH: 'cash'
+const ResourceType = {
+    BOT: 'BOT',
+    SERVER: 'SERVER',
+    MAINFRAME: 'MAINFRAME',
+    CASH: 'CASH',
+    SAM: "SAM"
 }
 
 var wallet = {}
 
-function add_resources(key, amount)
+
+add_resources(ResourceType.BOT, 1);
+add_resources(ResourceType.SERVER, 1);
+add_resources(ResourceType.BOT, 2);
+add_resources(ResourceType.CASH, 1);
+add_resources(ResourceType.SAM, 5);
+
+
+function add_resources(resourceType, amount)
 {
-    
+    if (!ResourceType.hasOwnProperty(resourceType))
+    {
+        console.log(resourceType + " is not a valid ResourceType! Please use `ResourceType.NAME`")
+        return;
+    }
+
+    if (!wallet.hasOwnProperty(resourceType)) wallet[resourceType] = 0;
+
+    wallet[resourceType] += amount;
+    console.log(resourceType + " | " + wallet[resourceType]);
 }
