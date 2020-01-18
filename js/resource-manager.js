cac2d89b3680ca9a1fe080d52f5747a8ba75da0e
samkent <kent_sam@outlook.com>
2020-01-18 17:59:42 +0000

    Fixed spelling.

--- js/resource-manager.js
+++ js/resource-manager.js
@@ -1,4 +1,9 @@
-//JS to make the resources pannel work
+//JS to make the resources panel work
 function updateResources(){
-    
+    //Get the resources panel as a variable.
+    let resourcespanel = document.getElementById("resources-");
+
+    ResourceType.forEach(element => {
+        
+    });
 }
