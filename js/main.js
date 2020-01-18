fddd94fab7df857f49fdddab27fb22f69682c425
rubyleehs <rubyleehs@live.com>
2020-01-18 13:52:36 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-const resources = {
+const resourceType = {
     BOT: 'bot',
     SERVER: 'server',
     MAINFRAME: 'mainframe',
     CASH: 'cash'
 }
 
+var wallet = {}
 
 function add_resources(key, amount)
 {
-
+    
 }
