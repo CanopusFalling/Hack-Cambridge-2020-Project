4ee1910a9bf2cb80f41854002ecef82bbcf46c8f
samkent <kent_sam@outlook.com>
2020-01-19 11:57:52 +0000

    Changed shop.

--- js/shopItems.js
+++ js/shopItems.js
@@ -5,7 +5,7 @@ var shopItems = [
         desc: "Why hack yourself when a bot can do it for you?",
         imgSrc: "pics/bot.png",
         price: {
-            COMPROMISED_DEVICE: 10
+            COMPROMISED_DEVICE: 2
         },
         reward: {
             physical: {
@@ -18,7 +18,7 @@ var shopItems = [
         desc: "You simply removed all the debugging stuff",
         imgSrc: "pics/bot.png",
         price: {
-            BOT: 10
+            COMPROMISED_DEVICE: 20
         },
         reward: {
             upgrade: {
@@ -36,7 +36,7 @@ var shopItems = [
         desc: "You take over a low security Guugal server",
         imgSrc: "pics/server.png",
         price: {
-            BOT: 30
+            BOT: 5
         },
         reward: {
             physical: {
@@ -45,11 +45,11 @@ var shopItems = [
         }
     },
     {
-        name: "Speed up overtaking of servers",
+        name: "Speed up overtaking of bots by servers",
         desc: "Use bots to make your life easier; no pain, all gain.",
         imgSrc: "pics/server.png",
         price: {
-            BOT: 40
+            BOT: 20
         },
         reward: {
             upgrade: {
@@ -57,7 +57,7 @@ var shopItems = [
                     factoryType: ResourceType.SERVER,
                     produceType: ResourceType.BOT,
                     multiplierType: IncrementValueType.BASE,
-                    value: 3
+                    value: 2
                 }
             }
         }
@@ -76,6 +76,24 @@ var shopItems = [
         }
     },
     {
+        name: "Speed up overtaking of servers by satellites",
+        desc: "Use satellites to take those servers on your behalf.",
+        imgSrc: "pics/satellitecol.png",
+        price: {
+            SERVER: 30
+        },
+        reward: {
+            upgrade: {
+                u1: {
+                    factoryType: ResourceType.SATELITE,
+                    produceType: ResourceType.SERVER,
+                    multiplierType: IncrementValueType.BASE,
+                    value: 2
+                }
+            }
+        }
+    },
+    {
         name: "Utilise AI",
         desc: "Use big data collected from satellites to develop AI",
         imgSrc: "pics/ai.png",
@@ -89,11 +107,29 @@ var shopItems = [
         }
     },
     {
+        name: "Use that AI to take some satellites.",
+        desc: "That AI can get you some sweet satellite loot",
+        imgSrc: "pics/ai.png",
+        price: {
+            SATELITE: 20
+        },
+        reward: {
+            upgrade: {
+                u1: {
+                    factoryType: ResourceType.AI,
+                    produceType: ResourceType.SATELLITE,
+                    multiplierType: IncrementValueType.BASE,
+                    value: 2
+                }
+            }
+        }
+    },
+    {
         name: "Create A Cyborg",
         desc: "An adorable mini-you to do all your warfare for you! It's all you've ever wanted!",
         imgSrc: "pics/cyborg.png",
         price: {
-            AI: 10
+            AI: 3
         },
         reward: {
             physical: {
@@ -102,15 +138,51 @@ var shopItems = [
         }
     },
     {
+        name: "Everyone knows Cyborgs beat AI",
+        desc: "Subjigate more AI with the help of your trusty cyborgs.",
+        imgSrc: "pics/cyborg.png",
+        price: {
+            AI: 20
+        },
+        reward: {
+            upgrade: {
+                u1: {
+                    factoryType: ResourceType.CYBORG,
+                    produceType: ResourceType.AI,
+                    multiplierType: IncrementValueType.BASE,
+                    value: 2
+                }
+            }
+        }
+    },
+    {
         name: "Hack a missile",
         desc: "It's like a pet fish! With bonus damage!",
         imgSrc: "pics/missile.png",
         price: {
-            CYBORG: 10
+            CYBORG: 4
         },
         reward: {
             physical: {
                 MISSILE: 1
+            }
+        }
+    },
+    {
+        name: "I'm out of excuses",
+        desc: "I don't know how but missiles somehow beat cyborgs. Increase their efficiency",
+        imgSrc: "pics/missile.png",
+        price: {
+            CYBORG: 20
+        },
+        reward: {
+            upgrade: {
+                u1: {
+                    factoryType: ResourceType.MISSILE,
+                    produceType: ResourceType.CYBORG,
+                    multiplierType: IncrementValueType.BASE,
+                    value: 2
+                }
             }
         }
     },
