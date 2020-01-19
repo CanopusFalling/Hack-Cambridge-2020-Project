7f6e84244088c68130324382b20248aaeac6450d
samkent <kent_sam@outlook.com>
2020-01-19 08:52:27 +0000

    Nice fixes.

--- js/resource-manager.js
+++ js/resource-manager.js
@@ -12,8 +12,8 @@ function updateResources()
         if (!ResourceType.hasOwnProperty(resource)) continue;
 
         //if(wallet[resource] != null && wallet[resource] != 0){
-        text = text + "<tr><th>" + getNicerResourceName(resource, wallet[resource]);
-        text = text + "</th><td> "
+        text = text + "<tr><td>" + getNicerResourceName(resource, wallet[resource]);
+        text = text + "</td><td> "
         /* if (rateOfGain[resource] != 0) */ text = text + rateOfGain[resource] + "/s";
         text = text + "</td></tr>";
         //}
@@ -39,6 +39,6 @@ document.addEventListener("DOMContentLoaded", function ()
 function getNicerResourceName(s, amount)
 {
     let sa = s.replace("_", " ").toLowerCase();
-    if (amount > 1 || amount < 1) sa = sa + "s";
+    if (amount > 1 || amount < -1) sa = sa + "s";
     return amount + " " + sa;
 }
