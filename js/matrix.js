ab4807d60b1202e0b5cb8c30eb0aa181fa87c05a
samkent <kent_sam@outlook.com>
2020-01-19 01:43:07 +0000

    Changed background.

--- js/matrix.js
+++ js/matrix.js
@@ -24,7 +24,7 @@ function draw()
 	ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
 	ctx.fillRect(0, 0, c.width, c.height);
 	
-	ctx.fillStyle = "#0F0"; //green text
+	ctx.fillStyle = "#F00"; //green text
 	ctx.font = font_size + "px arial";
 	//looping over drops
 	for(var i = 0; i < drops.length; i++)
